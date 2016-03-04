dc51fcc01fd80499e16071966aad2ab3563fa978
Jack Boyle <boylej98@hotmail.com>
2016-03-04 11:04:09 -0500

    Initial commit

---
+++ app/src/main/java/edu/tjhsst/jboyle/subclass/CustomTextView.java
@@ -0,0 +1,16 @@
+package edu.tjhsst.jboyle.subclass;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by jackpb1619 on 3/4/16.
+ */
+public class CustomTextView extends TextView
+{
+    public CustomTextView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
